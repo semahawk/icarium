0d69bb810358559e2f82ada94abcabe44e14db94
Szymon Urbaś <szymon.urbas@aol.com>
2019-01-24 23:45:18 +0100

    Fix the assembler (especially the `store` op)

--- tools/asm.py
+++ tools/asm.py
@@ -2,9 +2,11 @@ from lark import Lark, Transformer, v_args
 
 grammar = """
     start: instr*
-    ?instr: opcode reg "," imm ["shl" imm] -> instr_ris
-          | opcode reg "," reg ["off" imm] -> instr_rro
-          | opcode [imm] -> instr_i
+    instr: /set/ reg "," imm ["shl" imm] -> set
+          | /load/ reg "," reg ["off" imm] -> load
+          | /store/ reg "," reg ["off" imm] -> store
+          | /nop/ -> nop
+          | /halt/ -> halt
 
     !reg: /r[0-9]+/ -> reg
         | "pc" -> reg
@@ -15,12 +17,6 @@ grammar = """
         | /(0d)?[0-9]+/ -> imm
         | /0x[0-9a-fA-F]+/ -> imm
 
-    !opcode: "nop" -> nop
-           | "set" -> set
-           | "load" -> load
-           | "store" -> store
-           | "halt" -> halt
-
     COMMENT: /#(.*)/
 
     %import common.WS -> WS
@@ -30,15 +26,24 @@ grammar = """
 
 asm = """
 set r1, 0x00008010 shl 32
-set r2, 0x73 shl 0
-load r3, r1 off 0
+set r2, 0x73
+load r3, r1
 store r2, r1 off 0x10
-load r3, r1 off 0
+load r3, r1
 nop
 nop
 halt
 halt
 """
+
+def ris(opcode, variant, reg, imm, shl = 0):
+    return opcode << 57 | variant << 55 | reg << 50 | imm << 6 | shl
+
+def rro(opcode, variant, reg_dst, reg_src, off = 0):
+    return opcode << 57 | variant << 55 | reg_dst << 50 | reg_src << 45 | off
+
+def i(opcode, variant, imm):
+    return opcode << 57 | imm
 
 @v_args(inline = True)
 class Compiler(Transformer):
@@ -53,14 +58,13 @@ class Compiler(Transformer):
     def imm(self, *args):
         return int(self, base=0)
 
-    def nop(self):   return 0b0000000
-    def set(self):   return 0b0000001
-    def load(self):  return 0b0000010
-    def store(self): return 0b0000011
-    def halt(self):  return 0b1111111
+    def nop(self, *args):
+        return i(0b0000000, 0b00, 0)
 
-    def instr_ris(self, *args):
-        opcode = int(str(self), base = 0)
+    def halt(self, *args):
+        return i(0b1111111, 0b00, 0)
+
+    def set(self, *args):
         reg = args[0]
         imm = args[1]
 
@@ -69,12 +73,9 @@ class Compiler(Transformer):
         except:
             shl = 0
 
-        instr = opcode << 57 | reg << 50 | imm << 6 | shl
+        return ris(0b0000001, 0b00, reg, imm, shl)
 
-        return int(instr)
-
-    def instr_rro(self, *args):
-        opcode = int(str(self), base = 0)
+    def load(self, *args):
         reg_dst = args[0]
         reg_src = args[1]
 
@@ -83,21 +84,18 @@ class Compiler(Transformer):
         except:
             off = 0
 
-        instr = opcode << 57 | reg_dst << 50 | reg_src << 45 | off
+        return rro(0b0000010, 0b00, reg_dst, reg_src, off)
 
-        return instr
-
-    def instr_i(self, *args):
-        opcode = int(str(self), base = 0)
+    def store(self, *args):
+        reg_src = args[0]
+        reg_dst = args[1]
 
         try:
-            imm = args[0]
+            off = args[2]
         except:
-            imm = 0
+            off = 0
 
-        instr = opcode << 57 | imm
-
-        return instr
+        return rro(0b0000011, 0b00, reg_dst, reg_src, off)
 
     def start(self, *args):
         for instr in [self, *args]:
