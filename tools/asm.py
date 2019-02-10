c866c8be9dcc3d13f5f9bf197bf05698f5440b4c
Szymon Urbaś <szymon.urbas@aol.com>
2019-02-10 16:36:34 +0100

    tools/asm: Add .base and #pc

--- tools/asm.py
+++ tools/asm.py
@@ -17,69 +17,78 @@ prog_argp.add_argument('output',
 prog_args = prog_argp.parse_args()
 
 def ris(opcode, variant, reg, imm, shl = 0):
-    return opcode << 57 | variant << 55 | reg << 50 | imm << 6 | shl
+    return Instr(opcode << 57 | variant << 55 | reg << 50 | imm << 6 | shl)
 
 def rro(opcode, variant, reg_dst, reg_src, off = 0):
-    return opcode << 57 | variant << 55 | reg_dst << 50 | reg_src << 45 | off
+    return Instr(opcode << 57 | variant << 55 | reg_dst << 50 | reg_src << 45 | off)
 
 def i(opcode, variant, imm):
-    return opcode << 57 | imm
+    return Instr(opcode << 57 | imm)
+
+class Instr():
+    def __init__(self, encoding, emit = True):
+        self.encoding = encoding
+        self.emit = emit
 
 @v_args(inline = True)
 class Compiler(Transformer):
-    def reg(self, *args):
-        if self == "pc":
+    def __init__(self):
+        self.current_pc = 0x0
+
+    def base(self, op, base_addr):
+        new_pc = int(str(base_addr), base = 0)
+        self.current_pc = new_pc
+        # print("# setting current base addr to 0x{:x}".format(new_pc))
+        return Instr(new_pc, emit = False)
+
+    def reg(self, reg):
+        if reg == "pc":
             reg_id = 31
         else:
-            reg_id = str(self)[1:]
+            reg_id = int(str(reg)[1:])
 
-        return int(reg_id)
+        return reg_id
 
-    def imm(self, *args):
-        return int(self, base=0)
+    def imm(self, value):
+        return int(str(value), base = 0)
 
-    def nop(self, *args):
+    def special_imm(self, imm):
+        if (imm == "#pc"):
+            # print("# current pc is 0x{:x}".format(self.current_pc))
+            return self.current_pc
+        else:
+            raise Exception("unknown special immediate value: {}".format(imm))
+
+    def nop(self, op):
+        # print("{}".format(op))
+        self.current_pc += 8
         return i(0b0000000, 0b00, 0)
 
-    def halt(self, *args):
+    def halt(self, op):
+        # print("{}".format(op))
+        self.current_pc += 8
         return i(0b1111111, 0b00, 0)
 
-    def set(self, *args):
-        reg = args[0]
-        imm = args[1]
-
-        try:
-            shl = args[2]
-        except:
-            shl = 0
-
+    def set(self, op, reg, imm, shl = 0):
+        # print("{} r{}, 0x{:x} shl {}".format(op, reg, imm, shl))
+        self.current_pc += 8
         return ris(0b0000001, 0b00, reg, imm, shl)
 
-    def load(self, *args):
-        reg_dst = args[0]
-        reg_src = args[1]
-
-        try:
-            off = args[2]
-        except:
-            off = 0
-
+    def load(self, op, reg_dst, reg_src, off = 0):
+        # print("{} r{}, r{} off {}".format(op, reg_dst, reg_src, off))
+        self.current_pc += 8
         return rro(0b0000010, 0b00, reg_dst, reg_src, off)
 
-    def store(self, *args):
-        reg_src = args[0]
-        reg_dst = args[1]
-
-        try:
-            off = args[2]
-        except:
-            off = 0
-
+    def store(self, op, reg_src, reg_dst, off = 0):
+        # print("{} r{}, r{} off {}".format(op, reg_src, reg_dst, off))
+        self.current_pc += 8
         return rro(0b0000011, 0b00, reg_dst, reg_src, off)
 
-    def start(self, *args):
-        for instr in [self, *args]:
-            print("{:016x}".format(instr), file = prog_args.output)
+    def start(self, *instr):
+        for instr in instr:
+            if instr.emit:
+                print("{:016x}".format(instr.encoding), file = prog_args.output)
+                self.current_pc = 8
 
-parser = Lark(open("asm.lark"), parser = 'lalr', transformer = Compiler)
+parser = Lark(open("asm.lark"), parser = 'lalr', transformer = Compiler())
 tree = parser.parse(prog_args.input.read())
